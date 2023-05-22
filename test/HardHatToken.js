42f1ad7a36513da3b62e552d3349219e65ccf73b
Dimitar Danailov <dimityr.danailov@gmail.com>
2023-05-22 21:29:17 +0300

    Should transfer tokens between accounts is a new method.

--- test/HardHatToken.js
+++ test/HardHatToken.js
@@ -1,15 +1,32 @@
 const {expect} = require('chai')
-const {ethers} = require('hardhat')
 
 describe('Token contract', () => {
+  let Token
+
+  beforeEach(async () => {
+    Token = await ethers.getContractFactory('HardHatToken')
+  })
+
   it('Deployment should assign the total supply of tokens to the owner', async () => {
     const [owner] = await ethers.getSigners()
-
-    const Token = await ethers.getContractFactory('HardHatToken')
 
     const hardhatToken = await Token.deploy()
 
     const ownerBalance = await hardhatToken.balanceOf(owner.address)
     expect(await hardhatToken.totalSupply()).to.equal(ownerBalance)
+  })
+
+  it('Should transfer tokens between accounts', async () => {
+    const [owner, addr1, addr2] = await ethers.getSigners()
+
+    const hardhatToken = await Token.deploy()
+
+    // Transfer 50 tokens from owner to addr1
+    await hardhatToken.transfer(addr1.address, 50)
+    expect(await hardhatToken.balanceOf(addr1.address)).to.equal(50)
+
+    // Transfer 50 tokens from addr1 to addr2
+    await hardhatToken.connect(addr1).transfer(addr2.address, 50)
+    expect(await hardhatToken.balanceOf(addr2.address)).to.equal(50)
   })
 })
