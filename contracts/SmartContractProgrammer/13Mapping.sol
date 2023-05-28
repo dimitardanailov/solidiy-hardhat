6c02624e42213940a4bc69e5a3eb1d3d682d2956
Dimitar Danailov <dimityr.danailov@gmail.com>
2023-05-28 21:11:41 +0300

    IterableMapping is a new contract.

--- contracts/SmartContractProgrammer/13Mapping.sol
+++ contracts/SmartContractProgrammer/13Mapping.sol
@@ -50,3 +50,26 @@ contract NestedMapping {
     delete nested[_addr][_i];
   }
 }
+
+contract IterableMapping {
+  mapping(address => uint) public balances;
+  mapping(address => bool) public inserted;
+  address[] keys;
+
+  function set(address _key, uint _val) external {
+    balances[_key] = _val;
+
+    if (!inserted[_key]) {
+      inserted[_key] = true;
+      keys.push(_key);
+    }
+  }
+
+  function getSize() external view returns (uint) {
+    return keys.length;
+  }
+
+  function first() external view returns (uint) {
+    return balances[keys[0]];
+  }
+}
