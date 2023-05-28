0144fb9f7ca0445c0b17a49fedd24987e3752c09
Dimitar Danailov <dimityr.danailov@gmail.com>
2023-05-28 07:43:44 +0300

    Example of usage: Lopper.

---
+++ contracts/DiamondContracts/01 - Looper/DiamondContract.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.18;
+
+import "@solidstate/contracts/proxy/diamond/SolidStateDiamond.sol";
+
+contract FuncDiamond is SolidStateDiamond {
+  
+}
