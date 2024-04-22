e9d204188e2b3f304369bc38d67a2fc226725944
Dimitar Danailov <dimityr.danailov@gmail.com>
2024-04-22 09:37:17 +0300

    Array and structs

---
+++ contracts/Cyfrin/01.SimpleStorage/03.SimpleStorage.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.18;
+
+contract SimpleStorage {
+  uint256 myFavoriteNumber; // 0
+  struct Person {
+    uint256 favoriteNumber;
+    string name;
+  }
+  Person public pat = Person(7, "Pat");
+  Person public anna = Person({
+    favoriteNumber: 3,
+    name: "Anna"
+  });
+
+  Person[] listOfPeople; // []
+
+  function store(uint256 _favoriteNumber) public {
+    myFavoriteNumber = _favoriteNumber;
+  }
+
+  function retrieve() public view returns(uint256) {
+    return myFavoriteNumber;
+  }
+
+  function addPerson(uint256 _favoriteNumber, string memory _name) public {
+    Person memory person = Person({favoriteNumber: _favoriteNumber, name: _name});
+    listOfPeople.push(person);
+  }
+}
