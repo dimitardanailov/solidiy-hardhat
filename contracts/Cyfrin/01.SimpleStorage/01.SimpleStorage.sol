4f039355e966e618f2375f131a6f09e65cdbabb9
Dimitar Danailov <dimityr.danailov@gmail.com>
2024-04-22 08:57:18 +0300

    Basic variables

---
+++ contracts/Cyfrin/01.SimpleStorage/01.SimpleStorage.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.18;
+
+contract SimpleStorage {
+  // Basic Types: boolean, uint, init, address, bytes
+  bool hasFavoriteNumber = true;
+  uint256 favoriteNumber = 88;
+  string favoriteNumberInText = "88";
+  bytes32 favoriteBytes32 = "cat";
+}
