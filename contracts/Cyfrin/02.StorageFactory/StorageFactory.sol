6557f5073fd8d7a4e4aa8b43a198de58f20f37d3
Dimitar Danailov <dimityr.danailov@gmail.com>
2024-04-23 09:03:03 +0300

    Interacting with contracts ABI

--- contracts/Cyfrin/02.StorageFactory/StorageFactory.sol
+++ contracts/Cyfrin/02.StorageFactory/StorageFactory.sol
@@ -4,9 +4,24 @@ pragma solidity ^0.8.18;
 import {SimpleStorage} from "./SimpleStorage.sol";
 
 contract StorageFactory {
-  SimpleStorage public simpleStorage;
+  SimpleStorage[] public listOfContracts;
 
   function createSimpleStorageContract() public {
-    simpleStorage = new SimpleStorage();
+    listOfContracts.push(new SimpleStorage());
+  }
+
+  function sfStore(uint256 _index, uint256 _favoriteNumber) public {
+    // Address
+    // ABI (technically a lie, you just need the function selector)
+    // ABI - Application Binary Interface
+    // SimpleStorage(address)
+    SimpleStorage mySimpleStorage = SimpleStorage(listOfContracts[_index]);
+    mySimpleStorage.store(_favoriteNumber);
+  }
+
+  function sfGet(uint256 _index) public view returns(uint256) {
+    SimpleStorage mySimpleStorage = listOfContracts[_index];
+    
+    return mySimpleStorage.retrieve();
   }
 }
