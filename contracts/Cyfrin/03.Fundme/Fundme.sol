4b2f51b367b86bf8af54dbdfee37592ecb2ce552
Dimitar Danailov <dimityr.danailov@gmail.com>
2024-04-23 11:27:05 +0300

    Solidity math

--- contracts/Cyfrin/03.Fundme/Fundme.sol
+++ contracts/Cyfrin/03.Fundme/Fundme.sol
@@ -5,14 +5,14 @@ pragma solidity ^0.8.18;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
-  uint256 public myValue = 1;
+  uint256 public minimumUsd = 5e18;
   address AggregatorV3InterfaceAddress = 0x694AA1769357215DE4FAC081bf1f309aDC325306;
   
   function fund() public payable {
     // Allow users to send $
     // Have a minimum $ sent
     // 1. How do we send ETH to this contract?
-    require(msg.value > 1e18, "didn't send enough wei"); // 1e18 = 1 ETH
+    require(getConversationRate(msg.value) > minimumUsd, "didn't send enough ETH"); // 1e18 = 1 ETH
 
     // What's a revert ?
     // Undo any actions that have been done, adnd send the remaining gas back
@@ -24,6 +24,13 @@ contract FundMe {
     (,int256 answer,,,) = priceFeed.latestRoundData();
 
     return uint256(answer * 1e10);
+  }
+
+  function getConversationRate(uint256 ethAmount) public view returns(uint256) {
+    uint256 ethPrice = getPrice();
+    uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+
+    return ethAmountInUsd;
   }
 
   function getVersion() public view returns (uint256) {
